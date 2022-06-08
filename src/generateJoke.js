0bf3a424d6930d9a111f39f39c4a128a07bb7e83
khan <khanetmoi@gmail.com>
2022-06-08 12:23:32 +0000

    Webpack installed

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke(){
+    return "I don't trust stairs they are always up to something"
+}
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/main.css'
+
+import generateJoke from "./generateJoke";
+
+console.log(generateJoke())
+console.log('I am okayy')
